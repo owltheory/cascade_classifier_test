b221cfb58785433f719713c33e2fb48002f5a876
Michael Shullick <owl@megascops.net>
2015-08-24 11:23:12 -0400

    Changed identifier.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 #include "opencv2/imgproc/imgproc.hpp"
 
 #include <iostream>
-#include <stdio.h>
 
 using namespace std;
 using namespace cv;
@@ -17,10 +16,8 @@ void detectAndDisplay( Mat frame );
 
 /** Global variables */
 String cascade_name;
-CascadeClassifier face_cascade;
-CascadeClassifier eyes_cascade;
 CascadeClassifier cascade;
-string window_name = "Capture - Face detection";
+string window_name = "Classifier test: ";
 RNG rng(12345);
 
 /** @function main */
@@ -35,8 +32,9 @@ int main( int argc, const char** argv )
    		cout << "Loading " << cascade_name << " ..." << endl;
    };
    
-   //-- 1. Load the cascades
+    //-- 1. Load the cascades
     if( !cascade.load( cascade_name ) ) {printf("--(!)Error loading classifier. Exiting\n"); return -1; };
+    window_name += cascade_name;
 
     //-- 2. Read the video stream
     capture = cvCaptureFromCAM( -1 );
@@ -62,25 +60,46 @@ int main( int argc, const char** argv )
 /** @function detectAndDisplay */
 void detectAndDisplay( Mat frame )
 {
-    std::vector<Rect> faces;
+    std::vector<Rect> objectsDetected;
     Mat frame_gray;
 
     cvtColor( frame, frame_gray, CV_BGR2GRAY );
     equalizeHist( frame_gray, frame_gray );
 
-    //-- Detect faces
-    cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+    //-- Run classifierC++:
+    // void CascadeClassifier::
+    // detectMultiScale(const Mat& image,
+    // vector<Rect>& objects,
+    // double scaleFactor=1.1,
+    // int minNeighbors=3,
+    // int flags=0,
+    // Size minSize=Size(),
+    // Size maxSize=Size())
+    cascade.detectMultiScale( frame_gray, objectsDetected, 1.1, 10 );
 
-    for( size_t i = 0; i < faces.size(); i++ )
+    for( size_t i = 0; i < objectsDetected.size(); i++ )
     {
-        Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
-        ellipse( frame, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+        Point center( objectsDetected[i].x + objectsDetected[i].width*0.5, objectsDetected[i].y + objectsDetected[i].height*0.5 );
+        //ellipse( frame, center, Size( objectsDetected[i].width*0.5, objectsDetected[i].height*0.5), 0, 0, 360, , 4, 8, 0 );
+        Point topLeft (objectsDetected[i].x, objectsDetected[i].y);
+        Point bottomRight(topLeft.x + objectsDetected[i].width, topLeft.y + objectsDetected[i].height);
+        rectangle( frame, topLeft, bottomRight, Scalar( 0, 0, 255 ), 3);
 
-        Mat faceROI = frame_gray( faces[i] );
-        std::vector<Rect> eyes;
+        /*
+        String topLabel = cascade_name;
+        topLabel += ": " + i;
+
+        int fontScale = objectsDetected[i].height / 150;
+        String bottomLabel =  " x ";
+        bottomLabel.push_back(objectsDetected[i].width);
+        bottomLabel.append(objectsDetected[i].height);
+        putText(frame, topLabel, topLeft, FONT_HERSHEY_PLAIN, fontScale, 1);
+        putText(frame, bottomLabel, Point (bottomRight.x - bottomLabel.length(), bottomRight.y), FONT_HERSHEY_PLAIN, fontScale, 1);
+        */
+
+
 
     }
     //-- Show what you got
-    cout << "Displaying ..." << endl;
     imshow( window_name, frame );
 }
